f4d1d0aa9b7e93c969409651a9cb90ebf5185f0a
Tom Rix <Tom@bumblecow.com>
2018-04-03 07:04:01 -0700

    Rework visitor decl to use pt class list

--- src/c/pt/pt_classes.h
+++ src/c/pt/pt_classes.h
@@ -32,95 +32,101 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef AFRONT_C_PT_CLASSES_H
-#define AFRONT_C_PT_CLASSES_H
 
-class abstract_array_declarator;
-class abstract_declarator;
-class abstract_function_declarator;
-class additive_expr;
-class alignment_specifier;
-class and_expr;
-class argument_expr_list;
-class array_declarator;
-class assignment_expr;
-class assignment_operator;
-class atomic_type_specifier;
-class block_item;
-class block_item_list;
-class cast_expr;
-class character_constant;
-class compound_statement;
-class conditional_expr;
-class constant;
-class constant_expr;
-class declaration;
-class declaration_list;
-class declaration_specifiers;
-class declarator;
-class direct_abstract_declarator;
-class direct_declarator;
-class enumeration_constant;
-class enumerator;
-class enumerator_list;
-class enum_specifier;
-class equality_expr;
-class exclusive_or_expr;
-class expr;
-class expression_statement;
-class external_definition;
-class floating_constant;
-class function_body;
-class function_declarator;
-class function_definition;
-class function_specifier;
-class generic_association;
-class generic_assoc_list;
-class generic_selection;
-class identifier;
-class identifier_list;
-class inclusive_or_expr;
-class init_declarator;
-class init_declarator_list;
-class initializer;
-class initializer_list;
-class integer_constant;
-class iteration_statement;
-class jump_statement;
-class labeled_statement;
-class logical_and_expr;
-class logical_or_expr;
-class m;
-class multiplicative_expr;
-class n;
-class parameter_declaration;
-class parameter_list;
-class parameter_type_list;
-class pointer;
-class postfix_expr;
-class primary_expr;
-class relation_expr;
-class selection_statement;
-class shift_expr;
-class specifier_qualifier_list;
-class statement;
-class statement_list;
-class static_assert_declaration;
-class storage_class_specifier;
-class string_literal;
-class struct_declaration;
-class struct_declaration_list;
-class struct_declarator;
-class struct_declarator_list;
-class struct_or_union;
-class struct_or_union_specifier;
-class translation_unit;
-class type_name;
-class type_qualifier;
-class type_qualifier_list;
-class type_specifier;
-class typedef_name;
-class unary_expr;
-class unary_operator;
+/* multiple includes is ok */
 
+// Define PT_CLASS(classname) to do something
+#ifndef PT_CLASS
+// If you did not, then it is foreward decl of the class
+#define PT_CLASS(C) class C;
 #endif
+
+PT_CLASS(abstract_array_declarator)
+PT_CLASS(abstract_declarator)
+PT_CLASS(abstract_function_declarator)
+PT_CLASS(additive_expr)
+PT_CLASS(alignment_specifier)
+PT_CLASS(and_expr)
+PT_CLASS(argument_expr_list)
+PT_CLASS(array_declarator)
+PT_CLASS(assignment_expr)
+PT_CLASS(assignment_operator)
+PT_CLASS(atomic_type_specifier)
+PT_CLASS(block_item)
+PT_CLASS(block_item_list)
+PT_CLASS(cast_expr)
+PT_CLASS(character_constant)
+PT_CLASS(compound_statement)
+PT_CLASS(conditional_expr)
+PT_CLASS(constant)
+PT_CLASS(constant_expr)
+PT_CLASS(declaration)
+PT_CLASS(declaration_list)
+PT_CLASS(declaration_specifiers)
+PT_CLASS(declarator)
+PT_CLASS(direct_abstract_declarator)
+PT_CLASS(direct_declarator)
+PT_CLASS(enumeration_constant)
+PT_CLASS(enumerator)
+PT_CLASS(enumerator_list)
+PT_CLASS(enum_specifier)
+PT_CLASS(equality_expr)
+PT_CLASS(exclusive_or_expr)
+PT_CLASS(expr)
+PT_CLASS(expression_statement)
+PT_CLASS(external_definition)
+PT_CLASS(floating_constant)
+PT_CLASS(function_body)
+PT_CLASS(function_declarator)
+PT_CLASS(function_definition)
+PT_CLASS(function_specifier)
+PT_CLASS(generic_association)
+PT_CLASS(generic_assoc_list)
+PT_CLASS(generic_selection)
+PT_CLASS(identifier)
+PT_CLASS(identifier_list)
+PT_CLASS(inclusive_or_expr)
+PT_CLASS(init_declarator)
+PT_CLASS(init_declarator_list)
+PT_CLASS(initializer)
+PT_CLASS(initializer_list)
+PT_CLASS(integer_constant)
+PT_CLASS(iteration_statement)
+PT_CLASS(jump_statement)
+PT_CLASS(labeled_statement)
+PT_CLASS(logical_and_expr)
+PT_CLASS(logical_or_expr)
+PT_CLASS(m)
+PT_CLASS(multiplicative_expr)
+PT_CLASS(n)
+PT_CLASS(parameter_declaration)
+PT_CLASS(parameter_list)
+PT_CLASS(parameter_type_list)
+PT_CLASS(pointer)
+PT_CLASS(postfix_expr)
+PT_CLASS(primary_expr)
+PT_CLASS(relation_expr)
+PT_CLASS(selection_statement)
+PT_CLASS(shift_expr)
+PT_CLASS(specifier_qualifier_list)
+PT_CLASS(statement)
+PT_CLASS(statement_list)
+PT_CLASS(static_assert_declaration)
+PT_CLASS(storage_class_specifier)
+PT_CLASS(string_literal)
+PT_CLASS(struct_declaration)
+PT_CLASS(struct_declaration_list)
+PT_CLASS(struct_declarator)
+PT_CLASS(struct_declarator_list)
+PT_CLASS(struct_or_union)
+PT_CLASS(struct_or_union_specifier)
+PT_CLASS(translation_unit)
+PT_CLASS(type_name)
+PT_CLASS(type_qualifier)
+PT_CLASS(type_qualifier_list)
+PT_CLASS(type_specifier)
+PT_CLASS(typedef_name)
+PT_CLASS(unary_expr)
+PT_CLASS(unary_operator)
+
+#undef PT_CLASS
