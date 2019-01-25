6194b79777ac223ea1974c59bc86a0e2ed8eb582
Tom Rix <tom.rix@sony.com>
2019-01-25 14:58:45 -0800

    Fix building flex issue

--- tools/scripts/flex.sh
+++ tools/scripts/flex.sh
@@ -9,9 +9,17 @@ V=2.6.4
 S=flex-${V}
 tar xf ../pkgs/${S}.tar.gz
 cd ${S}
+if [ -d build ]; then
+    rm -rf build
+fi
 mkdir build
 cd build
-../configure --prefix=$P
+
+# Problems building 2.6.4 will gcc 7.2
+# https://github.com/spack/spack/issues/6812
+# Is causing a build failure on Ubuntu 18.04
+# So switch to building with clang.
+CC=clang ../configure --prefix=$P
 if [ ! -f Makefile ]; then
     exit 1
 fi
